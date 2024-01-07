70cea5af400d547c68272cbbdbbf405981173774
Raymond Del Vecchio <raydelv@MackTruck.localdomain>
2024-01-06 18:47:59 -0800

    saving

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='vlite2',
-    version='1.1.0',
+    version='2.0.0',
     author='Ray Del Vecchio',
     author_email='ray@cerebralvalley.ai',
-    description='Improved simple vector database in Numpy. Original by @sdan.',
+    description='Improved simple vector database in Numpy, with many advanced proprietary features. Original by @sdan.',
     url='https://github.com/raydelvecchio/vlite-v2',
     packages=find_packages(),
     install_requires=[
