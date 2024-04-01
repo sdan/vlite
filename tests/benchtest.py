6b656245e7b996b3bdd9df03f73001b66bdc0417
Surya <git@sdan.io>
2024-03-31 22:11:37 -0700

    Add Chroma benchmarking code

--- tests/benchtest.py
+++ tests/benchtest.py
@@ -9,6 +9,9 @@ from vlite.main import VLite
 from vlite.utils import load_file, chop_and_chunk
 from vlite.model import EmbeddingModel
 
+import chromadb
+from chromadb.utils import embedding_functions
+
 def main(queries, corpuss, top_k, token_counts) -> pd.DataFrame:
     """Run the VLite benchmark.
 
@@ -35,16 +38,20 @@ def main(queries, corpuss, top_k, token_counts) -> pd.DataFrame:
 
     for corpus_idx, corpus in enumerate(corpuss):
         token_count = token_counts[corpus_idx]
-        print(f"Running VLite benchmark for corpus of size {token_count} tokens.")
+        print(f"Running benchmarks for corpus of size {token_count} tokens.")
 
-        if os.path.exists('vlite.npz'):
-            print("[+] Removing vlite.npz")
-            os.remove('vlite.npz')
+        #################################################
+        #                  VLite                        #
+        #################################################
+        
+        if os.path.exists('vlite-bench.npz'):
+            print("[+] Removing vlite-bench.npz")
+            os.remove('vlite-bench.npz')
 
         print("Adding vectors to VLite instance...")
         t0 = time.time()
 
-        vlite = VLite()
+        vlite = VLite("vlite-bench")
         result_add = vlite.add(corpus)
         t1 = time.time()
 
@@ -82,6 +89,61 @@ def main(queries, corpuss, top_k, token_counts) -> pd.DataFrame:
 
         print(json.dumps(results[-1], indent=2))
         print("Done VLite benchmark.")
+        
+        #################################################
+        #                  Chroma                       #
+        #################################################
+        print("Begin Chroma benchmark.")
+        print("Adding vectors to Chroma instance...")
+        t0 = time.time()
+
+        chroma_client = chromadb.Client()
+        sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="mixedbread-ai/mxbai-embed-large-v1")
+        collection = chroma_client.create_collection(name="my_collection", embedding_function=sentence_transformer_ef)        
+        ids = [str(i) for i in range(len(corpus))]
+        try:
+            collection.add(documents=corpus, ids=ids)
+        except Exception as e:
+            print(e)
+            print("Failed to add documents to Chroma collection.a")
+            t0 = time.time()
+
+        t1 = time.time()
+        print(f"Took {t1 - t0:.3f}s to add vectors.")
+        indexing_times.append(
+            {
+                "num_tokens": token_count,
+                "lib": "Chroma",
+                "num_embeddings": len(corpus),
+                "indexing_time": t1 - t0,
+            }
+        )
+
+        print("Starting Chroma trials...")
+        times = []
+
+        for query_vector in queries:
+            t0 = time.time()
+            try:
+                chroma_results = collection.query(
+                    query_texts=[query_vector], n_results=top_k)
+            except Exception as e:
+                print(e)
+                print("Failed to query Chroma collection.")
+                t0 = time.time()
+            print(f"Top {top_k} results: {chroma_results['distances']}")
+            t1 = time.time()
+            times.append(t1 - t0)
+
+        results.append(
+            {
+                "num_embeddings": len(corpus),
+                "lib": "Chroma",
+                "k": top_k,
+                "avg_time": np.mean(times),
+                "stddev_time": np.std(times),
+            }
+        )
 
     results_df = pd.DataFrame(results)
     indexing_times_df = pd.DataFrame(indexing_times)
@@ -98,13 +160,15 @@ if __name__ == "__main__":
         "How many parameters does Transformers have?",
         "What are the limitations of Transformers?",
     ]
-    corpus = load_file('data/attention.pdf')
+    corpus = load_file(os.path.join(os.path.dirname(__file__), 'data/attention.pdf'))
     chopped_corpus = chop_and_chunk(text=corpus)
-    chopped_corpus = " ".join(chopped_corpus)
-    token_count = EmbeddingModel().token_count(chopped_corpus)
+    token_count = EmbeddingModel().token_count(" ".join(chopped_corpus))
 
-    benchmark_corpuss = [chopped_corpus, chopped_corpus*2, chopped_corpus*4]
-    benchmark_token_counts = [token_count, token_count*2, token_count*4]
+    # benchmark_corpuss = [chopped_corpus, chopped_corpus*2, chopped_corpus*4]
+    # benchmark_token_counts = [token_count, token_count*2, token_count*4]
+    
+    benchmark_corpuss = [chopped_corpus]
+    benchmark_token_counts = [token_count]
 
     print("Token count:", token_count)
     print("Corpus length:", len(chopped_corpus))
